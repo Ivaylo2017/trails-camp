2636d844cff396d1eb4f72292fb76a0d657df033
Ivaylo2017 <ivoaleksiev@yahoo.com>
2022-04-22 23:48:33 -0400

    Adding to the Single Page App

--- src/shared/campsites.js
+++ src/shared/campsites.js
@@ -1,138 +1,34 @@
 export const CAMPSITES = [
     {
         id: 0,
-        name: 'React Lake Campground',
-        image: 'assets/images/react-lake.jpg',
+        name: "React Lake Campground",
+        image: "/assets/images/react-lake.jpg",
         elevation: 1233,
-        description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers.",
-        comments: [
-            {
-                author: "Tinus Lorvaldes",
-                text: "What a magnificent view!",
-                date: "Oct 25, 2018"
-            },
-            {
-                author: "Brennen Ech",
-                text: "The campground was beautiful, but the bugs could bite sometimes.",
-                date: "Jun 16, 2017"
-            },
-            {
-                author: "McKenzie Sebastian",
-                text: "I caught a world-record-breaking trout here on my last visit.",
-                date: "Feb 18, 2019"
-            },
-            {
-                author: "Jordan Runn",
-                text: "A wonderful place to reconnect with nature.",
-                date: "Aug 04, 2019"
-            },
-            {
-                author: "Ann Dabramov",
-                text: "The stars at night were magnificent!",
-                date: "Jul 23, 2018"
-            }
-        ]
+        featured: false,
+        description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers."
     },
     {
         id: 1,
-        name: 'Chrome River Campground ',
-        image: 'assets/images/chrome-river.jpg',
+        name:"Chrome River Campground ",
+        image: "/assets/images/chrome-river.jpg",
         elevation: 877,
-        description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River.",
-        comments: [
-            {
-                author: "Tinus Lorvaldes",
-                text: "What a magnificent view!",
-                date: "Oct 25, 2018"
-            },
-            {
-                author: "Brennen Ech",
-                text: "The campground was beautiful, but the bugs could bite sometimes.",
-                date: "Jun 16, 2017"
-            },
-            {
-                author: "McKenzie Sebastian",
-                text: "I caught a world-record-breaking trout here on my last visit.",
-                date: "Feb 18, 2019"
-            },
-            {
-                author: "Jordan Runn",
-                text: "A wonderful place to reconnect with nature.",
-                date: "Aug 04, 2019"
-            },
-            {
-                author: "Ann Dabramov",
-                text: "The stars at night were magnificent!",
-                date: "Jul 23, 2018"
-            }
-        ]
+        featured: false,
+        description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River."
     },
     {
         id: 2,
-        name: 'Breadcrumb Trail Campground',
-        image: 'assets/images/breadcrumb-trail.jpg',
+        name:"Breadcrumb Trail Campground",
+        image: "/assets/images/breadcrumb-trail.jpg",
         elevation: 2901,
-        description: "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground.",
-        comments: [
-            {
-                author: "Tinus Lorvaldes",
-                text: "What a magnificent view!",
-                date: "Oct 25, 2018"
-            },
-            {
-                author: "Brennen Ech",
-                text: "The campground was beautiful, but the bugs could bite sometimes.",
-                date: "Jun 16, 2017"
-            },
-            {
-                author: "McKenzie Sebastian",
-                text: "I caught a world-record-breaking trout here on my last visit.",
-                date: "Feb 18, 2019"
-            },
-            {
-                author: "Jordan Runn",
-                text: "A wonderful place to reconnect with nature.",
-                date: "Aug 04, 2019"
-            },
-            {
-                author: "Ann Dabramov",
-                text: "The stars at night were magnificent!",
-                date: "Jul 23, 2018"
-            }
-        ]
+        featured: false,
+        description: "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground."
     },
     {
         id: 3,
-        name: 'Redux Woods Campground',
-        image: 'assets/images/redux-woods.jpg',
+        name:"Redux Woods Campground",
+        image: "/assets/images/redux-woods.jpg",
         elevation: 42,
-        description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods.",
-        comments: [
-            {
-                author: "Tinus Lorvaldes",
-                text: "What a magnificent view!",
-                date: "Oct 25, 2018"
-            },
-            {
-                author: "Brennen Ech",
-                text: "The campground was beautiful, but the bugs could bite sometimes.",
-                date: "Jun 16, 2017"
-            },
-            {
-                author: "McKenzie Sebastian",
-                text: "I caught a world-record-breaking trout here on my last visit.",
-                date: "Feb 18, 2019"
-            },
-            {
-                author: "Jordan Runn",
-                text: "A wonderful place to reconnect with nature.",
-                date: "Aug 04, 2019"
-            },
-            {
-                author: "Ann Dabramov",
-                text: "The stars at night were magnificent!",
-                date: "Jul 23, 2018"
-            }
-        ]
+        featured: true,
+        description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
     }
-]
+];
