f347ac387c983cf5d3c37c3f76f212ab4247334c
Ivaylo2017 <ivoaleksiev@yahoo.com>
2022-04-30 14:09:02 -0400

    Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle,
         ModalHeader, ModalBody, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from "./LoadingComponent";
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -18,10 +19,8 @@ class  CommentForm extends Component {
     }
 
     handleSubmit = (values) => {
-        console.log('Current state is: ' + JSON.stringify(values));
-        alert('Current state is: ' + JSON.stringify(values));
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, vaues.rating, values.author, values.text);
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     toggleModal = () => {
@@ -122,6 +121,26 @@ function RenderCampsite({campsite}){
     }
 
    function CampsiteInfo(props){
+       if (props.isLoading) {
+           return (
+               <div className="container" >
+                   <div className="row">
+                       <Loading />
+                   </div>
+               </div>
+           );
+        }
+        if (props.errMess) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <h4>{props.errMess}</h4>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
         if(props.campsite) {
             return (
                 <div className="container">
